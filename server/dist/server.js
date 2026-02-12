0b0494a81e49197b4abc2ab2fde6024f8630df84
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2026-02-12 03:05:56 -0500

    Finished setting up Prisma ORM for backend

--- server/dist/server.js
+++ server/dist/server.js
@@ -3,13 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const PORT = 3001;
+//const PORT:Number | undefined = process.env.PORT;
 app.get("/", (request, response) => {
-    response.send("Hello TreNyce");
+    response.send(`Hello TreNyce ${process.env.PRISMA_DATABASE_URL}`);
 });
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
 //# sourceMappingURL=server.js.map
